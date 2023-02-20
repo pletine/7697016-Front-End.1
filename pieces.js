cf8e0fec48effc97cf38f432481a5929cc5d3b61
pletine <pierre.letinevez@gmail.com>
2023-02-20 12:03:33 +0400

    Ajout de boutons pour filtrer les pièces Création de fonctions pour filtrer les pièces et sélectionner selon certaines conditions Ajout de listes pré-filtrées dans les fiches

--- pieces.js
+++ pieces.js
@@ -42,3 +42,99 @@ for (let i = 0; i < pieces.length; i++) {
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(dispoElement);
 }
+
+// Tri par prix croissant sur le bouton dédié
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+});
+
+// Tri par prix décroissant sur le bouton dédié
+const boutonTrierDecroi = document.querySelector(".btn-trier-decroi");
+boutonTrierDecroi.addEventListener("click", function () {
+    const piecesOrdonneesDecroi = Array.from(pieces);
+    piecesOrdonneesDecroi.sort(function (a, b) {
+        return b.prix - a.prix;
+    });
+    console.log(piecesOrdonneesDecroi);
+});
+
+// Filtre sur le prix abordables des pièces après appui sur le bouton dédié
+const boutonFiltrerAbor = document.querySelector(".btn-filtrer");
+boutonFiltrerAbor.addEventListener("click", function () {
+   const piecesFiltrees = pieces.filter(function (piece) {
+       return piece.prix <= 35;
+   });
+   console.log(piecesFiltrees);
+});
+
+// Filtre sur le prix non abordables des pièces après appui sur le bouton dédié
+const boutonFiltrerNoAbor = document.querySelector(".btn-filtrer-abord");
+boutonFiltrerNoAbor.addEventListener("click", function () {
+   const piecesFiltrees = pieces.filter(function (piece) {
+       return piece.prix > 35;
+   });
+   console.log(piecesFiltrees);
+});
+
+// Filtrer les pièces sans description
+const boutonFiltrerNoDescr = document.querySelector(".nodescr");
+boutonFiltrerNoDescr.addEventListener("click", function () {
+   const piecesFiltrees = pieces.filter(function (piece) {
+       return piece.description;
+   });
+   console.log(piecesFiltrees);
+});
+
+// Exemple autre
+const prix_doubles = pieces.map(piece => piece.prix * 2);
+
+// Récupération des noms des pièces uniquement
+const noms = pieces.map(piece => piece.nom);
+// Et suppression des pièces non abordables
+for(let i = pieces.length -1 ; i >= 0; i--){
+   if(pieces[i].prix > 35){
+       noms.splice(i,1);
+   }
+}
+// console.log(noms);
+
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+   const nomElement = document.createElement('li');
+   nomElement.innerText = noms[i];
+   abordablesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+   .appendChild(abordablesElements)
+
+
+
+// Afficher les pièces disponibles
+const noms_dispo = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(!pieces[i].dispo){
+        noms_dispo.splice(i,1);
+    }
+    else {
+        noms_dispo[i] += ` - ${pieces[i].prix} €`;
+    }
+}
+console.log(noms_dispo);
+
+const dispoElements = document.createElement('ul');
+for(let i=0; i < noms_dispo.length ; i++){
+   const nomElement = document.createElement('li');
+   nomElement.innerText = noms_dispo[i];
+   dispoElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.disponibles')
+   .appendChild(dispoElements)
